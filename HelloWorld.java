45720c2e0e2aadcedab78d99e74a53faa1fb07fa
Poulbz <Poul@Shabos-MacBook-Pro.local>
2016-10-26 23:55:48 -0400

    third

--- HelloWorld.java
+++ HelloWorld.java
@@ -15,7 +15,7 @@ public Apple(){
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World!22222");
+		System.out.println("Hello World!3333");
 		
 		Apple a=new Apple();
 		
